5d25ee23662e93d9edb14d1aa4a25d2f910d50f8
ozambelachota <haroldozambelachota@outlook.com>
2024-11-20 23:38:54 -0500

    modify home public result

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,6 +11,10 @@ import {
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import Confetti from "react-confetti";
+import { useNavigate } from "react-router-dom";
+import { clientApi } from "../api/client.api";
+import { userAdmin } from "../services/api.service";
+import { useUserStore } from "../store/login.store";
 const grupos = [
   {
     grupo: 1,
@@ -104,6 +108,26 @@ export default function Home() {
     window.addEventListener("resize", handleResize); // Escuchar cambios de tamaño
     return () => window.removeEventListener("resize", handleResize); // Limpieza
   }, []);
+  const setUser = useUserStore((state) => state.setUserData);
+  const navigate = useNavigate();
+  const rol = useUserStore((state) => state.rol);
+
+  useEffect(() => {
+    clientApi.auth.getSession().then(({ data }) => {
+      if (data.session) {
+        userAdmin(data.session.user.id).then((rol) => {
+          if (rol)
+            setUser(
+              data.session.user.user_metadata.full_name ?? "",
+              data.session.user.user_metadata.picture ?? "",
+              rol,
+              data.session.user.id ?? ""
+            );
+        });
+        if (rol === "admin") navigate("/admin/home", { replace: true });
+      }
+    });
+  }, [navigate, rol, setUser]);
 
   const groupColors = [
     "#FFCDD2",
